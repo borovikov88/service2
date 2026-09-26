e333847e97f9a4ab1f3df70afe68fb24c578c522
borovikov88 <71123182+borovikov88@users.noreply.github.com>
2026-09-26 23:03:36 +0700

    Add own reward participation proposal permission

--- pool_service/models.py
+++ pool_service/models.py
@@ -55,6 +55,7 @@ class Organization(models.Model):
             ("import_payroll", "Can import payroll"),
             ("manage_employee_mapping", "Can manage employee mapping"),
             ("view_employee_rewards", "Can view employee reward summaries"),
+            ("propose_employee_rewards", "Can propose own employee reward participation"),
             ("manage_employee_rewards", "Can manage employee reward participation"),
             ("manage_reward_rules", "Can manage employee reward rules"),
             ("close_reward_month", "Can close employee reward month"),
